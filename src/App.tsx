ec8b2a1b5b500a2f52a7de1a3e6ba0a38a21659c
azagatti <andre.zagatti1@gmail.com>
2020-04-22 01:05:16 -0300

    feat: create contexts and hooks

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,16 @@ import GlobalStyle from './styles/global';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 
-const App: React.FC = () => {
-  return (
-    <>
+import AppProvider from './hooks';
+
+const App: React.FC = () => (
+  <>
+    <AppProvider>
       <SignIn />
-      <GlobalStyle />
-    </>
-  );
-};
+    </AppProvider>
+
+    <GlobalStyle />
+  </>
+);
 
 export default App;
